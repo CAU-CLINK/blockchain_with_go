0c28e3d5ffabefebf473d1ea304f0c596251a69c
EuiJoon-Lee <ejl1@naver.com>
2019-01-23 19:16:28 +0900

    difficulty readjust logic

---
+++ consensus/difficulty.go
@@ -0,0 +1,16 @@
+package consensus
+
+// New difficulty = Old difficulty * (actual time of last 2016 blocks / 20160 minutes)
+// 20160min == 1209600sec
+// Max adjustment -> factor of 4
+
+
+//check whether current block is multiple of 2016
+func Check2016() {
+
+}
+
+//if current block height is multiple of 2016, adjust block difficulty
+func DifficultyAdjustment() {
+
+}
