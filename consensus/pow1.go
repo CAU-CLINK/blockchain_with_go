b4a018212064f7c1196eed12406c85b5a8479373
EuiJoon-Lee <ejl1@naver.com>
2019-01-23 19:18:22 +0900

    New pow logic

---
+++ consensus/pow1.go
@@ -0,0 +1,27 @@
+package consensus
+
+//Pseudo code
+
+LastBlockHeader
+CurrentBlockHeader
+
+var maxNonce = math.MaxInt32 // original bitcoin spec
+
+const targetbits = CurrenBlockHeader.Bits
+
+type ProofOfWork struct {
+	Header *CurrentBlockHeader
+	target *big.Int
+}
+
+func prepareData() {
+
+}
+
+func Run() {
+
+}
+
+func Validate() {
+	
+}
