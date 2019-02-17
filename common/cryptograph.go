abd168945d05aa582ea624dd397f23ca8f3f760c
KIMSJ <leesd556@gmail.com>
2019-02-17 16:10:54 +0900

    Implements cryptograph function

--- common/cryptograph.go
+++ common/cryptograph.go
@@ -1,1 +1,45 @@
 package common
+
+import (
+	"github.com/minio/sha256-simd"
+	"golang.org/x/crypto/ripemd160"
+	"log"
+	"github.com/btcsuite/btcutil/base58"
+)
+
+const version = 0x00
+const checksumLength = 4
+
+func Base58CheckEncode(payload []byte) string {
+	versionedPayload := append([]byte{version}, payload...)
+	checksum := Checksum(versionedPayload)
+
+	fullyPayload := append(versionedPayload, checksum...)
+	encodedPayload := base58.Encode(fullyPayload)
+
+	return encodedPayload
+}
+
+func PubkeyHash(pubKey []byte) []byte {
+	pubkeySHA256 := sha256.Sum256(pubKey)
+
+	RIPEMD160 := ripemd160.New()
+	_, err := RIPEMD160.Write(pubkeySHA256[:])
+	if err != nil {
+		log.Panic(err)
+	}
+	pubkeyRIPEMD160 := RIPEMD160.Sum(nil)
+
+	return pubkeyRIPEMD160
+}
+
+func Checksum(hash160 []byte) []byte {
+	hashed := doubleHash(hash160)
+	return hashed[:checksumLength]
+}
+
+func doubleHash(payload []byte) []byte {
+	fisrt := sha256.Sum256(payload)
+	second := sha256.Sum256(fisrt[:])
+	return second[:]
+}
