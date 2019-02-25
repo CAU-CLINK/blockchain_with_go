b7d702e4977ea500c9a665ecb8f98a35cfe19046
KIMSJ <leesd556@gmail.com>
2019-02-25 23:55:26 +0900

    change UTXO skel

--- blockchain/transaction.go
+++ blockchain/transaction.go
@@ -1,6 +1,13 @@
 package blockchain
 
-import "github.com/CAU-CLINK/blockchain_with_go/script"
+import (
+	"fmt"
+
+	"github.com/CAU-CLINK/blockchain_with_go/common"
+	"github.com/CAU-CLINK/blockchain_with_go/script"
+	"github.com/CAU-CLINK/blockchain_with_go/wallet"
+	"github.com/minio/sha256-simd"
+)
 
 const subsidy = 10
 
@@ -12,13 +19,45 @@ type Transaction struct {
 }
 
 // TODO: Implements me with test case
-func NewTransaction() *Transaction {
-	return nil
+func NewTransaction(wallet *wallet.Wallet, to string, amount uint, utxos UTXOs) *Transaction {
+	var inputs []TxInput
+	var outputs []TxOutput
+	var acc uint = 0
+
+	pubKeyHash := common.PubkeyHash(wallet.PublicKey.ToBytes())
+
+	for key, utxo := range utxos {
+		acc += utxo.Value()
+		/*
+			Neet to sign here!
+		*/
+		input := TxInput{key.TxID(), key.Vout(), script.ScriptSig{nil, pubKeyHash}}
+		inputs = append(inputs, input)
+	}
+
+	from := fmt.Sprintf("%s", wallet.GetAddress())
+	outputs = append(outputs, NewTxOutput(amount, to))
+	if acc > amount {
+		outputs = append(outputs, NewTxOutput(acc-amount, from)) // a change
+	}
+
+	tx := Transaction{inputs, outputs}
+
+	return &tx
 }
 
 // TODO: Implements me with test case
-func (tx *Transaction) Hash() []byte {
-	return nil
+func (tx Transaction) Hash() ([]byte, error) {
+	var hash [32]byte
+
+	serializedTx, err := common.Serialize(tx)
+	if err != nil {
+		return nil, err
+	}
+
+	hash = sha256.Sum256(serializedTx)
+
+	return hash[:], nil
 }
 
 func NewCoinbase(to string) *Transaction {
