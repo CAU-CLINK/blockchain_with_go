f36152995bb422cdbdad26a79f46d33f0e786986
김성재 <kim@gimseongjaeui-MacBookPro.local>
2019-03-24 15:18:59 +0900

    Add utxo update test case

--- blockchain/utxo_test.go
+++ blockchain/utxo_test.go
@@ -3,6 +3,8 @@ package blockchain_test
 import (
 	"testing"
 
+	"github.com/CAU-CLINK/blockchain_with_go/wallet"
+
 	"github.com/CAU-CLINK/blockchain_with_go/common"
 
 	"github.com/btcsuite/btcutil/base58"
@@ -59,12 +61,12 @@ func testUTXOset(address string) error {
 
 		utxo := blockchain.NewUTXO(db.transaction.TxOut[0], 0)
 
-		serialiezUTXO, err := common.Serialize(&utxo)
+		serialiezedUTXO, err := common.Serialize(&utxo)
 		if err != nil {
 			return err
 		}
 
-		chainstate.Put(utxoKey.Bytes(), serialiezUTXO)
+		chainstate.Put(utxoKey.Bytes(), serialiezedUTXO)
 	}
 
 	chainstate.Close()
@@ -156,6 +158,54 @@ func TestUTXOSet_FindUTXOs(t *testing.T) {
 	}
 }
 
+// Scenario
+// wallet1 has utxo 10, 10, 10, 10, 10
+// wallet1 sends 10 utxo to testAddress
+// tx output is 10 value to testAddress and 40 value to wallet1 (change)
+// utxoset has 2 utxos : wallet1 has 40 utxo, testAddress has 10 utxo
 func TestUTXOSet_Update(t *testing.T) {
+	wallet1, err := wallet.New()
+	if err != nil {
+		t.Error(err)
+	}
 
+	err = testUTXOset(wallet1.Address())
+	if err != nil {
+		t.Error(err)
+	}
+
+	defer os.RemoveAll(utxoSetPath)
+
+	utxoSet, err := blockchain.NewUTXOSet(utxoSetPath)
+	if err != nil {
+		t.Error(err)
+	}
+
+	utxos, err := utxoSet.FindUTXOs(wallet1.PubKeyHash(), 50)
+	if err != nil {
+		t.Error(err)
+	}
+
+	tx, _ := blockchain.NewTransaction(wallet1.PublicKey.Bytes(), testAddress, 10, utxos)
+
+	var txs []*blockchain.Transaction
+	txs = append(txs, tx)
+
+	block := blockchain.NewBlock(nil, txs)
+
+	utxoSet.Update(block)
+
+	testAddrPubkeyHash := base58.Decode(testAddress)
+	testAddrPubkeyHash = testAddrPubkeyHash[1 : len(testAddrPubkeyHash)-4]
+
+	wallet1UTXOs, err := utxoSet.FindUTXOs(wallet1.PubKeyHash(), 40)
+	testAddrUTXOs, err := utxoSet.FindUTXOs(testAddrPubkeyHash, 10)
+
+	if len(wallet1UTXOs) != 1 {
+		t.Error("Invalid utxos!")
+	}
+
+	if len(testAddrUTXOs) != 1 {
+		t.Error("Invalid utxos!")
+	}
 }
